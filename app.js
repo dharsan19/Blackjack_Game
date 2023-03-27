aa74c118c19738b3395cce9deccc0df2220555bf
Dharsan <dharsan19082002@gmail.com>
2023-03-27 21:46:43 +0530

    feat: files added

---
+++ app.js
@@ -0,0 +1,6 @@
+const notesList = document.querySelector('.notes ul');
+const noteTitle = document.querySelector('#note-title');
+const noteContent = document.querySelector('#note-content');
+const noteAttachment = document.querySelector('#note-attachment');
+const noteTags = document.querySelector('#note-tags');
+const noteForm = document.querySelector('.note-details form');
